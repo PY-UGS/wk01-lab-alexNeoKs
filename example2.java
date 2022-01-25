f0517dd8e9bf9c360115a00c0e85de854adb67fc
alexNeoKs <92634671+alexNeoKs@users.noreply.github.com>
2022-01-25 10:24:42 +0800

    Add files via upload

---
+++ example2.java
@@ -0,0 +1,23 @@
+public class example2 {
+    public static void main(String arg[]){
+
+    String modulecode = "CSC1009";
+    switch(modulecode){
+        case "CSC1009":
+            System.out.println("Object-Oriented Programming");
+            break;
+
+        case "CSC1006":
+            System.out.println("Math 2");
+            break;
+        case "CSC1007":
+            System.out.println("Operating systems");
+            break;
+        case "CSC1008":
+            System.out.println("Data structures and algorithms");
+            break;
+    }
+
+ } 
+
+}
---
+++ example3.java
@@ -0,0 +1,11 @@
+public class example3 {
+    public static void main(String[] args){
+
+        for(int x = 102 ;  x  > 66; x = x - 1){
+            if (x%2!=0){
+                System.out.println("The value of x is: " + x);
+            }
+        }
+
+    }
+}
